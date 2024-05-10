09b15109a792429ffdbd6df400b9daf720ba2934
Rafael Costa <thealulafilms@gmail.com>
2024-05-10 13:56:04 -0300

    Upload main project

---
+++ Principal/src/main/java/com/a3/principal/Principal.java
@@ -0,0 +1,9 @@
+package com.a3.principal;
+
+public class Principal
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Ficlkomg World!");
+    }
+}
---
+++ Principal/src/main/java/com/a3/principal/Produto.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.a3.principal;
+
+/**
+ *
+ * @author Pichau
+ */
+public class Produto {
+    
+}
